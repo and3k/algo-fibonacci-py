88414809f674ebc73adb0282dbe5cb8e54a65822
and3k <andrewjkasten@gmail.com>
2025-11-12 21:10:33 -0700

    fibonacci solution

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,13 @@
 def fibonacci(n):
-  pass
+  f = [0,1]
+  if n == 0:
+    return 0
+  if n == 1:
+    return 1
+  #print(2 - 1 + 2 - 2)
+  for seq in range(2, n + 1):
+    total = f[seq - 1] + f[seq - 2]
+    f.append(total)
+  return f[n]
 
 
